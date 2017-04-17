0e57aa26d58574cff1ef9a331d984e044b23f892
Tyler Payne <tpayne95@gmail.com>
2017-04-16 22:21:42 -0400

    upload

---
+++ Generator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public Vector3[] generators;
+    public int maxIteration;
+    public Material material;
+    public float width;
+}
