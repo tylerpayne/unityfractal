f8d4f23762907f341a1b8598f7db60d2ed0201d2
Tyler Payne <tpayne95@gmail.com>
2017-04-16 22:50:05 -0400

    update; pictures

--- Fractal.cs
+++ Fractal.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Fractal : MonoBehaviour
 {
-    public float terminator, speed;
+    public float length;
     public Generator generators;
 
     public int iteration = 0;
@@ -14,6 +14,11 @@ public class Fractal : MonoBehaviour
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        if (lineRenderer == null)
+        {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+            lineRenderer.useWorldSpace = false; 
+        }
         if (iteration == 0)
         {
             Generate();
@@ -34,20 +39,20 @@ public class Fractal : MonoBehaviour
                 Fractal child = Instantiate(this);
                 child.iteration++;
                 child.transform.position = transform.position;
-                child.transform.position += transform.forward * (terminator * generator.x);
-                child.transform.position += transform.up * (generator.y * terminator);
-                child.transform.position += transform.right * (generator.z * terminator);
+                child.transform.position += transform.forward * (length * generator.x);
+                child.transform.position += transform.up * (generator.y * length);
+                child.transform.position += transform.right * (generator.z * length);
 
                 Vector3 term;
                 if (i == count - 1)
                 {
-                    term = transform.position + transform.forward.normalized * terminator;
+                    term = transform.position + transform.forward.normalized * length;
                 }
                 else
                 {
                     term = children[i + 1];
                 }
-                child.terminator = (term - child.transform.position).magnitude;
+                child.length = (term - child.transform.position).magnitude;
                 child.transform.LookAt(term);
                 children[i] = child.transform.position;
                 child.gameObject.SetActive(true);
@@ -57,14 +62,14 @@ public class Fractal : MonoBehaviour
             iteration++;
             Vector3 fvec = children[0] - transform.position;
             transform.LookAt(children[0]);
-            terminator = fvec.magnitude;
+            length = fvec.magnitude;
 
         }
     }
 
     void Update()
     {
-        Vector3 end = new Vector3(0, 0, terminator);
+        Vector3 end = new Vector3(0, 0, length);
         lineRenderer.SetPosition(1, end);
         lineRenderer.material = generators.material;
         lineRenderer.startWidth = generators.width;
